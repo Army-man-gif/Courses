dbc862c6c1a333dbf5e1b9ec130bb58080cec30f
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-04-26 23:58:01 +0100

    .

--- Course 2 - Android App with Python/Kivy_app_2.py
+++ Course 2 - Android App with Python/Kivy_app_2.py
@@ -5,10 +5,10 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 class SpartanGrid(GridLayout):
-
+   
     def __init__(self,**kwargs):
         super(SpartanGrid, self).__init__()
-        self.cols = 3
+        self.cols = 2
         self.add_widget(Label(text="Student Name: "))
 
         self.s_name = TextInput(multiline=True)
@@ -21,6 +21,15 @@ class SpartanGrid(GridLayout):
         self.add_widget(Label(text="Student Gender"))
         self.s_gender = TextInput()
         self.add_widget(self.s_gender)
+
+        self.press = Button(text="Click Me")
+        self.press.bind(on_press=self.click_me)
+        self.add_widget(self.press)
+    def click_me(self,instance):
+        print("Name of student is: "+self.s_name.text)
+        print("Marks of student are: "+self.s_marks.text)
+        print("Gender of student is: "+self.s_gender.text)
+
 class SpartanApp(App):
     def build(self):
         return SpartanGrid()
