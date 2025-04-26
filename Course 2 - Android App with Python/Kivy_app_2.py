09640c0060822f53d55049ed8f1cc90c0f01e885
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-04-26 23:49:28 +0100

    .

--- Course 2 - Android App with Python/Kivy_app_2.py
+++ Course 2 - Android App with Python/Kivy_app_2.py
@@ -7,17 +7,21 @@ class SpartanGrid(GridLayout):
 
     def __init__(self,**kwargs):
         super(SpartanGrid, self).__init__()
-
+        self.cols = 
         self.add_widget(Label(text="Student Name: "))
 
-        self.s_name = TextInput(multiline=False)
+        self.s_name = TextInput(multiline=True)
         self.add_widget(self.s_name)
 
+        self.add_widget(Label(text="Student Marks: "))
+        self.s_marks = TextInput(multiline=True)
+        self.add_widget(self.s_marks)
 
+        self.add_widget(Label(text="Student Gender"))
 class SpartanApp(App):
     def build(self):
-        return SpartanGrid
+        return SpartanGrid()
     
-    
+
 if __name__ == "__main__":
     SpartanApp().run()
