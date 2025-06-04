243897f00733543e556214015f616eea0f1df3ad
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-04 23:37:26 +0100

    sdf

--- WebDevNothingToPro/Getting Started Modules/My first website/scripts/main.js
+++ WebDevNothingToPro/Getting Started Modules/My first website/scripts/main.js
@@ -22,5 +22,12 @@ let myHeading = document.querySelector("h1")
 function setUserName() {
   const myName = prompt("Please enter your name.");
   localStorage.setItem("name", myName);
-  myHeading.textContent = `Mozilla is cool, ${myName}`;
+  myHeading.textContent = `Your name is, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
 }
