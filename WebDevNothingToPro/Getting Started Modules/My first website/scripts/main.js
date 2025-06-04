563de42d074ffb7f73d6e5a622a36a5f2ddd5b2b
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-04 23:35:13 +0100

    gfg

--- WebDevNothingToPro/Getting Started Modules/My first website/scripts/main.js
+++ WebDevNothingToPro/Getting Started Modules/My first website/scripts/main.js
@@ -14,3 +14,13 @@ function func(e){
 }
 
 const listen = Image.addEventListener("click",func);
+
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1")
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
