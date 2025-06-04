68fa0541fe4b5329f9b174959c9d5338ade24af1
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-04 18:01:27 +0100

    gsgsdfsf

--- WebDevNothingToPro/Getting Started Modules/My first website/basicinteractivity.js
+++ WebDevNothingToPro/Getting Started Modules/My first website/basicinteractivity.js
@@ -1,11 +1,7 @@
 const listItems = document.querySelectorAll("li");
 
 function toggleDone(e) {
-  if (!e.target.className) {
-    e.target.className = "done";
-  } else {
-    e.target.className = "";
-  }
+  e.target.classList.toggle("done");
 }
 
 listItems.forEach((item) => {
