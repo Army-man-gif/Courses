855b2b5d3f6a4cbd5f391f596f71eb67648f8d2c
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-19 23:20:37 +0100

    sdfs

--- WebDevNothingToPro/React_app/moz-todo-react/src/App.jsx
+++ WebDevNothingToPro/React_app/moz-todo-react/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 
+
+const subject = "REACT";
 function App() {
   return (
     <>
       <header>
-        <h1>I'm trying to make a React app for the first time, assisted by the "vite" build tool</h1>
+        <h1>I'm trying to make a {subject} app for the first time, assisted by the "vite" build tool</h1>
         <button type="button" className="primary"> Click me! </button>
+        <p>Hello, {`${subject} :)`}!</p>
+        <p>Hello, {subject.toUpperCase()}</p>
+        <p>Hello, {2 + 2}!</p>
       </header>
 
     </>
