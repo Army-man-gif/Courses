448971b888cd1304c5b11d099dd8e2c403fd9777
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-20 00:07:46 +0100

    sdfdsfsf

--- WebDevNothingToPro/React_app/moz-todo-react/src/App.jsx
+++ WebDevNothingToPro/React_app/moz-todo-react/src/App.jsx
@@ -6,10 +6,10 @@ function App(props) {
   return (
     <>
       <header>
-        <h1>I'm trying to make a {subject} app for the first time, assisted by the "vite" build tool</h1>
+        {/*<h1>I'm trying to make a {subject} app for the first time, assisted by the "vite" build tool</h1>*/}
         <button type="button" className="primary"> Click me! </button>
-        <p>Hello, {`${subject} :)`}!</p>
-        <p>Hello, {subject.toUpperCase()}</p>
+        {/*<p>Hello, {`${subject} :)`}!</p>
+        <p>Hello, {subject.toUpperCase()}</p>*/}
         <p>Hello, {2 + 2}!</p>
       </header>
 
