4ed1d0d798cd581ee23cdd7c114e4fd0a5754d19
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-19 23:15:00 +0100

    sdffsdfs

--- WebDevNothingToPro/React_app/moz-todo-react/src/App.jsx
+++ WebDevNothingToPro/React_app/moz-todo-react/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <header>
         <h1>I'm trying to make a React app for the first time, assisted by the "vite" build tool</h1>
-        <button type="button">Click me!</button>
+        <button type="button" className="primary"> Click me! </button>
       </header>
 
     </>
