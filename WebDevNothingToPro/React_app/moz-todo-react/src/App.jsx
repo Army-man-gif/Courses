eeccb94098f82339c755a63eaf67caf10d672275
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-20 00:27:49 +0100

    sfsfsf

--- WebDevNothingToPro/React_app/moz-todo-react/src/App.jsx
+++ WebDevNothingToPro/React_app/moz-todo-react/src/App.jsx
@@ -2,11 +2,11 @@ import './App.css'
 
 
 function App(props) {
-  console.log(props)
   return (
     <>
       <header>
         {/*<h1>I'm trying to make a {subject} app for the first time, assisted by the "vite" build tool</h1>*/}
+        <h1>{props.subject}</h1>
         <button type="button" className="primary"> Click me! </button>
         {/*<p>Hello, {`${subject} :)`}!</p>
         <p>Hello, {subject.toUpperCase()}</p>*/}
