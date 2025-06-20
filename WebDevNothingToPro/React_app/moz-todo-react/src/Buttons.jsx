c8aefbcb7a842efd5a04cacb88c8d319742e610c
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-21 00:46:25 +0100

    sfdsfsfsgs

--- WebDevNothingToPro/React_app/moz-todo-react/src/Buttons.jsx
+++ WebDevNothingToPro/React_app/moz-todo-react/src/Buttons.jsx
@@ -1,19 +1,34 @@
 function Button(){
     <div className="filters btn-group stack-exception">
+
         <button type="button" className="btn toggle-btn" aria-pressed="true">
+
           <span className="visually-hidden">Show </span>
+
           <span>all</span>
+
           <span className="visually-hidden"> tasks</span>
+
         </button>
+
         <button type="button" className="btn toggle-btn" aria-pressed="false">
+
           <span className="visually-hidden">Show </span>
+
           <span>Active</span>
+
           <span className="visually-hidden"> tasks</span>
+
         </button>
+
         <button type="button" className="btn toggle-btn" aria-pressed="false">
+
           <span className="visually-hidden">Show </span>
+
           <span>Completed</span>
+
           <span className="visually-hidden"> tasks</span>
+
         </button>
       </div>
 }
