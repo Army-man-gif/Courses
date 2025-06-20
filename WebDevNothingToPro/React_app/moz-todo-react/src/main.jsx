3e4e6a29b42d425a6747b175d18288b9ce919258
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-21 00:50:57 +0100

    sfsf

--- WebDevNothingToPro/React_app/moz-todo-react/src/main.jsx
+++ WebDevNothingToPro/React_app/moz-todo-react/src/main.jsx
@@ -1,11 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import {Greeting} from './Greeting.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App subject="Clarice"/>
-    <i><Greeting/></i>
   </StrictMode>,
 )
