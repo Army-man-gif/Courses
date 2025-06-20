c9499078a6257fe0516c0424cbe9b37039630f05
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-21 00:42:03 +0100

    sf

--- WebDevNothingToPro/React_app/moz-todo-react/src/App.jsx
+++ WebDevNothingToPro/React_app/moz-todo-react/src/App.jsx
@@ -1,27 +1,13 @@
 import "./App_replacedcss.css"
 import Form from './Form.jsx'
+import Button from './Button.jsx'
+
 function App() {
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form/>
-      <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-      </div>
+      <Button/>
       <h2 id="list-heading">3 tasks remaining</h2>
       <ul
         role="list"
