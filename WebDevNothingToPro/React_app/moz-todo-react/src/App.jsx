69e15496fadc2e3a163e9891928a90d8e22871b3
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-20 23:29:39 +0100

    sfsf

--- WebDevNothingToPro/React_app/moz-todo-react/src/App.jsx
+++ WebDevNothingToPro/React_app/moz-todo-react/src/App.jsx
@@ -1,25 +1,10 @@
 import "./App_replacedcss.css"
+import {Greeting} from './Greeting.jsx'
 function App() {
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      
       <div className="filters btn-group stack-exception">
         <button type="button" className="btn toggle-btn" aria-pressed="true">
           <span className="visually-hidden">Show </span>
