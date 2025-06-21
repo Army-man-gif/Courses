21d46b6d1cd63f1a1f55137f76f76907eca9bfb3
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-21 20:31:21 +0100

    sffsd

--- WebDevNothingToPro/React_app/moz-todo-react/src/Task.jsx
+++ WebDevNothingToPro/React_app/moz-todo-react/src/Task.jsx
@@ -1,21 +1,31 @@
 import { useState } from "react";
 
-function Task({name,id,isChecked,toggleTaskCompleted,deleteTask}){
+function Task({name,id,isChecked,toggleTaskCompleted,deleteTask,editTask}){
   const [isEditing, setEditing] = useState(false);
+  const [newName, setNewName] = useState(name);
+  function handleSubmit(e) {
+    e.preventDefault();
+    editTask(props.id, newName);
+    setNewName("");
+    setEditing(false);
+  }
+  function handleChange(e) {
+    setNewName(e.target.value);
+  }
   const editTemplate = (
     <>
           <div className="c-cb">
-            <input id={id} type="checkbox" defaultChecked={isChecked} name={name} key={id} onChange={() => toggleTaskCompleted(id)} />
+            <input id={id} type="text" name={newName} key={id} onChange={handleChange} />
             <label className="todo-label" htmlFor={id}>
-              {name}
+              {newName}
             </label>
           </div>
           <div className="btn-group">
-            <button type="button" className="btn">
-              Cancel <span className="visually-hidden">renaming {name}</span>
+            <button type="button" className="btn" onClick={() => setEditing(false)}>
+              Cancel <span className="visually-hidden">renaming {newName}</span>
             </button>
-            <button type="button" className="btn btn__danger" onClick={() => deleteTask(id)} >
-              Save <span className="visually-hidden">renaming {name}</span>
+            <button type="button" className="btn btn__danger">
+              Save <span className="visually-hidden">renaming {newName}</span>
             </button>
           </div>
     </>
@@ -23,17 +33,17 @@ function Task({name,id,isChecked,toggleTaskCompleted,deleteTask}){
   const viewTemplate = (
     <>
           <div className="c-cb">
-            <input id={id} type="checkbox" defaultChecked={isChecked} name={name} key={id} onChange={() => toggleTaskCompleted(id)} />
+            <input id={id} type="checkbox" defaultChecked={isChecked} name={newName} key={id} onChange={() => toggleTaskCompleted(id)} />
             <label className="todo-label" htmlFor={id}>
-              {name}
+              {newName}
             </label>
           </div>
           <div className="btn-group">
             <button type="button" className="btn" onClick={() => setEditing(true)}>
-              Edit <span className="visually-hidden">{name}</span>
+              Edit <span className="visually-hidden">{newName}</span>
             </button>
             <button type="button" className="btn btn__danger" onClick={() => deleteTask(id)} >
-              Delete <span className="visually-hidden">{name}</span>
+              Delete <span className="visually-hidden">{newName}</span>
             </button>
           </div>
     </>
