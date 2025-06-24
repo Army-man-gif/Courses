9b440a76233472d5f2819535847d2cfa0acfdc72
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-24 23:37:00 +0100

    sdffs

--- WebDevNothingToPro/React_app/moz-todo-react/vite.config.js
+++ WebDevNothingToPro/React_app/moz-todo-react/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Courses/WebDevNothingToPro/React_app/mozo-todo-react/',
+  base: '/Courses/WebDevNothingToPro/React_app/mozo-todo-react/Build_files',
   plugins: [react()],
   server: {
     open: true,
