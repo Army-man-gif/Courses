46451b66dea516350e0cbde8af2a5ea2c3e5b547
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-26 23:53:54 +0100

    dsf

--- WebDevNothingToPro/React_app/moz-todo-react/src/Images.jsx
+++ WebDevNothingToPro/React_app/moz-todo-react/src/Images.jsx
@@ -2,7 +2,11 @@
 
 
 function Canvas(){
+    const canvas = document.querySelector("canvas");
+    const ctx = canvas.getContext("2d");
 
+    const width = (canvas.width = window.innerWidth);
+    const height = (canvas.height = window.innerHeight);
 
     return(
         <>
