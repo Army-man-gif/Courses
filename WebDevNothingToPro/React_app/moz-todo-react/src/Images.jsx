e13eaf1906d888a8ecbdac1cba662d256981fdff
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-26 23:53:20 +0100

    sdf

---
+++ WebDevNothingToPro/React_app/moz-todo-react/src/Images.jsx
@@ -0,0 +1,17 @@
+
+
+
+function Canvas(){
+
+
+    return(
+        <>
+            <h1>
+                Canvas
+            </h1>
+            <canvas>
+
+            </canvas>
+        </>
+    );
+}
