3a09fce5cb08cd4c0b4ad856f12d73299fd1e32b
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-27 21:10:51 +0100

    fsdf

--- WebDevNothingToPro/React_app/moz-todo-react/src/three.jsx
+++ WebDevNothingToPro/React_app/moz-todo-react/src/three.jsx
@@ -1,21 +1,41 @@
+import { useEffect } from 'react';
+import * as THREE from 'three';
+function Three(){
+    useEffect(() => {
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000,
+        );
+        camera.position.z = 5;
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement);
+        let cube;
 
-function three(){
+        const loader = new THREE.TextureLoader();
+
+        loader.load("metal003.png", (texture) => {
+        texture.wrapS = THREE.RepeatWrapping;
+        texture.wrapT = THREE.RepeatWrapping;
+        texture.repeat.set(2, 2);
+
+        const geometry = new THREE.BoxGeometry(2.4, 2.4, 2.4);
+        const material = new THREE.MeshLambertMaterial({ map: texture });
+        cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+
+        draw();
+        });
+    },[]);
+
     return (
         <>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r79/three.min.js"></script>
-            <script>
-                const scene = new THREE.Scene();
-                const camera = new THREE.PerspectiveCamera(
-                    75,
-                    window.innerWidth / window.innerHeight,
-                    0.1,
-                    1000,
-                );
-                camera.position.z = 5;
-            </script>
         </>
     )
 }
 
 
-export default three
+export default Three
