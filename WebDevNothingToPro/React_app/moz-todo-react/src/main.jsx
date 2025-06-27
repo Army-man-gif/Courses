7614c6b280443339e5877245aa56fed15f6ebbaf
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-27 20:58:38 +0100

    sdfs

--- WebDevNothingToPro/React_app/moz-todo-react/src/main.jsx
+++ WebDevNothingToPro/React_app/moz-todo-react/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import three from './three.jsx'
+import Three from './three.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <three />
+    <Three />
   </StrictMode>,
 )
