f9a95e0381b8c43428637b688ed978f5165530aa
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-27 20:57:26 +0100

    sdf

--- WebDevNothingToPro/React_app/moz-todo-react/src/three.jsx
+++ WebDevNothingToPro/React_app/moz-todo-react/src/three.jsx
@@ -1,10 +1,13 @@
-function three(){
-    const scene = new THREE.Scene();
 
+function three(){
 
     return (
         <>
-        
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r79/three.min.js"></script>
+            <script>
+                const scene = new THREE.Scene();
+                
+            </script>
         </>
     )
 }
