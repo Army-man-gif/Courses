3cca5a4a3b0fbe5cd7ce173229c39fe6a53fad71
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-27 21:00:59 +0100

    dfs

--- WebDevNothingToPro/React_app/moz-todo-react/src/three.jsx
+++ WebDevNothingToPro/React_app/moz-todo-react/src/three.jsx
@@ -6,7 +6,13 @@ function three(){
             <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r79/three.min.js"></script>
             <script>
                 const scene = new THREE.Scene();
-                
+                const camera = new THREE.PerspectiveCamera(
+                    75,
+                    window.innerWidth / window.innerHeight,
+                    0.1,
+                    1000,
+                );
+                camera.position.z = 5;
             </script>
         </>
     )
