90d763eb5e57ebe7a9c65cd1f631b0e407aaecb3
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-28 17:07:11 +0100

    '.'

--- WebDevNothingToPro/React_app/moz-todo-react/src/ThirdPartyAPI.jsx
+++ WebDevNothingToPro/React_app/moz-todo-react/src/ThirdPartyAPI.jsx
@@ -2,6 +2,12 @@
 function APIs(){
     return (
         <>
+            <script
+                src="https://api.mqcdn.com/sdk/mapquest-js/v1.3.2/mapquest.js" defer>
+            </script>
+            <link
+            rel="stylesheet"
+            href="https://api.mqcdn.com/sdk/mapquest-js/v1.3.2/mapquest.css" />
         </>
     )
 }
