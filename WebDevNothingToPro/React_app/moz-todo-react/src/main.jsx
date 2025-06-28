6b2e132f4849e73da456ab7dcd0600c76b4238b0
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-28 19:14:05 +0100

    '.'

--- WebDevNothingToPro/React_app/moz-todo-react/src/main.jsx
+++ WebDevNothingToPro/React_app/moz-todo-react/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Alarm from './alarmAPI.jsx'
+import BasicAnimation from './BasicsCSSAnimation.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Alarm />
+    <BasicAnimation />
   </StrictMode>,
 )
