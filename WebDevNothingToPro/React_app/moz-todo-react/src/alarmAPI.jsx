e28a13572c4709b808a153e14a1731b10495e2b6
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-28 18:44:00 +0100

    '.'

--- WebDevNothingToPro/React_app/moz-todo-react/src/alarmAPI.jsx
+++ WebDevNothingToPro/React_app/moz-todo-react/src/alarmAPI.jsx
@@ -1,8 +1,14 @@
-
+import { useEffect } from "react";
 
 function Alarm(){
+    useEffect(() => {
+
+
+    },[])
     return(
         <>
+            <button id="set-alarm">Set alarm</button>
+            <div id="output"></div>
         </>
     )
 }
