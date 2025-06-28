33363dcc016d8a3427d69b5b12484d3e75742537
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-28 18:44:42 +0100

    '.'

--- WebDevNothingToPro/React_app/moz-todo-react/src/alarmAPI.jsx
+++ WebDevNothingToPro/React_app/moz-todo-react/src/alarmAPI.jsx
@@ -2,7 +2,16 @@ import { useEffect } from "react";
 
 function Alarm(){
     useEffect(() => {
+        const output = document.querySelector("#output");
+        const button = document.querySelector("#set-alarm");
 
+        function setAlarm() {
+            setTimeout(() => {
+                output.textContent = "Wake up!";
+            }, 1000);
+        }
+
+        button.addEventListener("click", setAlarm);
 
     },[])
     return(
