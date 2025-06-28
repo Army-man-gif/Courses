18256bd5af9d42b6092f18a8b541d859feb5d10a
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-28 16:49:07 +0100

    '.'

--- WebDevNothingToPro/React_app/moz-todo-react/src/BasicsCSSAnimeation.jsx
+++
@@ -1,9 +0,0 @@
-
-
-function BasicAnimation() {
-    return(
-        <>
-        <img src={import.meta.env.BASE_URL +"/download (1).jpeg"}></img>
-        </>
-    )
-}
--- WebDevNothingToPro/React_app/moz-todo-react/src/main.jsx
+++ WebDevNothingToPro/React_app/moz-todo-react/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Three from './three.jsx'
+import BasicAnimation from './BasicsCSSAnimation.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Three />
+    <BasicAnimation />
   </StrictMode>,
 )
