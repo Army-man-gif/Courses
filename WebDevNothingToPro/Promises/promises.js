706143407ac5a6ae2f9390d8f5e6240d4377bccd
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-28 18:14:33 +0100

    '.'

--- WebDevNothingToPro/Promises/promises.js
+++ WebDevNothingToPro/Promises/promises.js
@@ -24,3 +24,23 @@ fetchPromise2
   .catch((error) => {
     console.error(`Could not get products: ${error}`);
   });
+
+    const fetchPromise3 = fetch(
+        "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+    );
+    const fetchPromise4 = fetch(
+        "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found",
+    );
+    const fetchPromise5 = fetch(
+        "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json",
+    );
+
+Promise.all([fetchPromise3, fetchPromise4, fetchPromise5])
+  .then((responses) => {
+    for (const response of responses) {
+      console.log(`${response.url}: ${response.status}`);
+    }
+  })
+  .catch((error) => {
+    console.error(`Failed to fetch: ${error}`);
+  });
