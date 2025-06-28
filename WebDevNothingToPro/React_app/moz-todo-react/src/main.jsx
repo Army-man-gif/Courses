64f097a053ab7f4bde4e8adbd5242f51ee387b36
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-28 17:13:42 +0100

    '.'

--- WebDevNothingToPro/React_app/moz-todo-react/src/main.jsx
+++ WebDevNothingToPro/React_app/moz-todo-react/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import BasicAnimation from './BasicsCSSAnimation.jsx'
+import APIs from './ThirdPartyAPI.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BasicAnimation />
+    <APIs />
   </StrictMode>,
 )
