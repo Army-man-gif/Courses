1117f9f9170f3dfb3ae20cc9286645b8a3503376
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-28 18:06:27 +0100

    '.'

--- WebDevNothingToPro/Promises/promises.js
+++ WebDevNothingToPro/Promises/promises.js
@@ -14,9 +14,13 @@ const fetchPromise2 = fetch(
   "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
 );
 
-fetchPromise2.then((response) => {
-  const jsonPromise = response.json();
-  jsonPromise.then((data) => {
+fetchPromise2
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
     console.log(data[0].name);
   });
-})
