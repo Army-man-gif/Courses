51b019e8b008bdbf0caf886dac8237d7d61fcbd3
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-06-28 18:24:27 +0100

    '.'

--- WebDevNothingToPro/Promises/promises.js
+++ WebDevNothingToPro/Promises/promises.js
@@ -16,12 +16,15 @@ const fetchPromise2 = fetch(
 
 fetchPromise2
   .then((response) => {
+      console.log("Trying Promise individual thens: ");
     return response.json();
   })
   .then((data) => {
+    console.log("Trying Promise individual thens: ");
     console.log(data[0].name);
   })
   .catch((error) => {
+    console.log("Trying Promise individual thens: ");
     console.error(`Could not get products: ${error}`);
   });
 
@@ -37,11 +40,13 @@ fetchPromise2
 
   Promise.all([fetchPromise3, fetchPromise4, fetchPromise5])
     .then((responses) => {
+      console.log("Trying Promise.all, Good URLs: ");
       for (const response of responses) {
         console.log(`${response.url}: ${response.status}`);
       }
     })
     .catch((error) => {
+      console.log("Trying Promise.all, Good URLs: ");
       console.error(`Failed to fetch: ${error}`);
     });
 
@@ -57,11 +62,13 @@ fetchPromise2
 
   Promise.all([fetchPromise6, fetchPromise7, fetchPromise8])
     .then((responses) => {
+      console.log("Trying Promise.all, bad URLs: ");
       for (const response of responses) {
         console.log(`${response.url}: ${response.status}`);
       }
     })
     .catch((error) => {
+      console.log("Trying Promise.all: ");
       console.error(`Failed to fetch: ${error}`);
     });
 
@@ -77,8 +84,10 @@ fetchPromise2
 
   Promise.any([fetchPromise9, fetchPromise10, fetchPromise11])
     .then((response) => {
+      console.log("Trying Promise.any: ");
       console.log(`${response.url}: ${response.status}`);
     })
     .catch((error) => {
+      console.log("Trying Promise.any: ");
       console.error(`Failed to fetch: ${error}`);
     });
