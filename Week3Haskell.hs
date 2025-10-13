5ce98b9b85da975b5b9898070dff74428c6b86d2
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-10-13 18:29:12 +0100

    sdf

--- Week3Haskell.hs
+++ Week3Haskell.hs
@@ -24,6 +24,8 @@ concat xss = [x | xs <- xss, x <- xs]
 firsts :: [(a,b)] -> [a]
 firsts ps = [x | (x, _) <- ps]
 
+length :: [a] -> Int
+length xs = sum [1 | _ <- xs]
 
 main :: IO()
-main = printdependentGenerators
+main = print(Main.length [1,2,3,4,5,6]) 
