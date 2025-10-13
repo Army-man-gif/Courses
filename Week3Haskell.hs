bc077abd22533f0b42a6e4760ca980c06b89dfc3
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-10-13 16:58:59 +0100

    fs

--- Week3Haskell.hs
+++ Week3Haskell.hs
@@ -1,2 +1,12 @@
 -- List comprehensions
 import Data.Char
+firstListComprehension = [x^2 | x <- [1..5]]
+printFirstListComprehension = print(firstListComprehension)
+
+secondListComprehension = [(x,y) | x <- [1,2,3], y <- [4,5]]
+printsecondListComprehension = print(secondListComprehension)
+
+
+
+main :: IO()
+main = printsecondListComprehension
