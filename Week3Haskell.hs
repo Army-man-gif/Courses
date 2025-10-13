447e372ab8cb649655115c965fbcadebf781f7af
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-10-13 17:39:34 +0100

    sdf

--- Week3Haskell.hs
+++ Week3Haskell.hs
@@ -3,11 +3,21 @@ import Data.Char
 firstListComprehension = [x^2 | x <- [1..5]]
 printFirstListComprehension = print(firstListComprehension)
 
+-- Comprehensions can have multiple generators, separated by commas. For example:
+
 secondListComprehension = [(x,y) | x <- [1,2,3], y <- [4,5]]
 printsecondListComprehension = print(secondListComprehension)
+
+-- Changing the order of the generators changes the order of the elements in the final list:
 
 thirdListComprehension = [(x,y) | y <- [4,5], x <- [1,2,3]]
 printthirdListComprehension = print([(x,y) | y <- [4,5], x <- [1,2,3]])
 
+-- Later generators can depend on the variables that are introduced by earlier generators:
+
+--dependentGenerators = [(x,y) | x <- [1..3], y <- [x..2]]
+dependentGenerators = [(x,y) | x <- [1.1,1.2..3.9], y <- [x..2.9]]
+printdependentGenerators = print(dependentGenerators)
+
 main :: IO()
-main = printthirdListComprehension
+main = printdependentGenerators
