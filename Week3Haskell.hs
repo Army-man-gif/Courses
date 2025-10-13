0a348488324ef7ac07442a6bef5362f3e0678dd0
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-10-13 17:01:00 +0100

    sdf

--- Week3Haskell.hs
+++ Week3Haskell.hs
@@ -6,7 +6,8 @@ printFirstListComprehension = print(firstListComprehension)
 secondListComprehension = [(x,y) | x <- [1,2,3], y <- [4,5]]
 printsecondListComprehension = print(secondListComprehension)
 
-
+thirdListComprehension = [(x,y) | y <- [4,5], x <- [1,2,3]]
+printthirdListComprehension = print([(x,y) | y <- [4,5], x <- [1,2,3]])
 
 main :: IO()
-main = printsecondListComprehension
+main = printthirdListComprehension
