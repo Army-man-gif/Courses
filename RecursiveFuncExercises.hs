15bef3bbf1dc1233fa9be60acbcf385a0cdf7985
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-10-14 21:30:46 +0100

    sdf

--- RecursiveFuncExercises.hs
+++ RecursiveFuncExercises.hs
@@ -1,8 +1,22 @@
+-- Exercises
+
+-- Exercise 1
 and :: [Bool] -> Bool
 and []  = True
 and (x:xs) 
     | x == False = False
     | otherwise  = Main.and xs
 
+
+
+-- Exercise 2
+concat :: [[a]] -> [a]
+concat [] = []
+concat (x:xs) = x ++ Main.concat xs
+
+
+
 main  :: IO()
-main  = print(Main.and [])
+main  = print(Main.concat [[1,2], [3,4], [5]] )
+
+
