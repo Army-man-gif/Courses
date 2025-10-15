efce4ce601385b48c3af5a118506f95d42d24a5a
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-10-15 20:53:22 +0100

    sfd

---
+++ HigherOrderFunctions.hs
@@ -0,0 +1,8 @@
+import Data.Char
+-- Programming example : Binary String Transmitter
+
+import Data.Char
+type Bit = Int
+bin2int :: [Bit] -> Int
+bin2int bits = sum [w*b | (w,b) <- zip weights bits]
+               where weights = iterate (*2) 1
