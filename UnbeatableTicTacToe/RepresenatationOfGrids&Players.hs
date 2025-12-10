9c9184459e44b2d5a35c18a4603fe26375ce2f00
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-12-10 20:06:08 +0000

    sfd

--- UnbeatableTicTacToe/RepresenatationOfGrids&Players.hs
+++ UnbeatableTicTacToe/RepresenatationOfGrids&Players.hs
@@ -109,3 +109,27 @@ getNat prompt = do putStr prompt
                    else
                       do putStrLn "ERROR: Invalid number"
                          getNat prompt
+
+-- Human vs. human
+tictactoe :: IO ()
+tictactoe = run empty O
+
+run :: Grid -> Player -> IO ()
+run g p = do cls
+             goto (1,1)
+             putGrid g
+             run' g p
+
+run' :: Grid -> Player -> IO ()
+run' g p | wins O g  = putStrLn "Player O wins!\n"
+         | wins X g  = putStrLn "Player X wins!\n"
+         | full g    = putStrLn "It's a draw!\n"
+         | otherwise =
+              do i <- getNat (prompt p)
+                 case move g i p of
+                    []   -> do putStrLn "ERROR: Invalid move"
+                               run' g p
+                    [g'] -> run g' (next p)
+
+prompt :: Player -> String
+prompt p = "Player " ++ show p ++ ", enter your move: "
